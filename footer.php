96166c199e94b5b127bae415faaffc01a27ab3ac
jcagentzero <jundycosmod@gmail.com>
2018-08-24 22:31:08 +0800

    Changed the name of the theme from Latte to latteX.
    Signed-off-by: jcagentzero <jundycosmod@gmail.com>

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
 		<footer class="footer" id="footer">
 			<div class="row">
 				<div class="col-md-12">
-					<?php echo '<p><a target="_blank" href="http://www.hardeepasrani.com/portfolio/latte/">Latte</a><br/>'.__('Proudly powered by','latte').' WordPress</p>'; ?>
+					<?php echo '<p><a target="_blank" href="https://github.com/jundycosmod/latte">latteX</a><br/>' . __('Proudly powered by', 'latte') . ' WordPress</p>'; ?>
 				</div>
 			</div>
 		</footer>
 
 	</div>
-<?php wp_footer(); ?>
+<?php wp_footer();?>
  </body>
 </html>
